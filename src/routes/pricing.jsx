aa2b1d40d226fad3f1a8c29577ad502b212c5a6d
lucaschennn <lucasch@umich.edu>
2023-04-28 15:08:04 -0700

    pricing text and formatting

--- src/routes/pricing.jsx
+++ src/routes/pricing.jsx
@@ -67,6 +67,22 @@ function Pricing() {
                 </h4>
                 <OtherDescription active={active.other}/>
             </div>
+            <div className="important-info">
+            IMPORTANT POLICY INFORMATION!
+
+Payment and Fees:
+The full charge is specified as per the full pricing policy here unless otherwise discussed prior to the shoot.
+Payment is due during the time of booking
+Venmo, PayPal, and cash are accepted!
+
+Cancellation and Reschedule Policy:
+I understand that unforeseen circumstances may occur. As such, rescheduling may be done free of charge within 24 hours of the original event start time. Otherwise a $25 rescheduling fee will be included to your final charge.
+Cancellations made before 48 hours will receive a full refund. Cancellations made after will not receive a refund.
+
+Turnaround and Delivery
+Edited photos will be delivered within 2 weeks of the session. Larger events may take longer. A more accurate timeframe will be provided after booking.
+Rush delivery is available for $20-$40 depending on delivery date and the number of images.
+            </div>
         </div>
       </>
     );
@@ -99,25 +115,48 @@ function GradDescription(props) {
 
 function EventsDescription(props) {
     return ( props.active &&
-        <>
-            events description
-        </>
+        <div className="pricing-info">
+            <ul className="pricing-list">
+                <li> $40 flat fee (editing and transportation)</li>
+                <li> $40 per hour</li>
+                <li> around 20 edited photos per hour</li>
+            </ul>
+            <p className="pricing-list">
+                Please contact me regarding specific circumstances!
+            </p>
+        </div>
     )
 }
 
 function HeadshotsDescription(props) {
     return ( props.active &&
-        <>
-            headshots description
-        </>
+        <div className="pricing-info">
+            <h5 className="pricing-subheader">
+                Individual: $20
+            </h5>
+            <ul className="pricing-list">
+                <li> 30 minutes</li>
+                <li> 1 locations</li>
+                <li> 10+ edited photos</li>
+            </ul>
+            <h5 className="pricing-subheader">
+                Group: $15 per preson
+            </h5>
+            <ul className="pricing-list">
+                <li> 2+ people minutes</li>
+                <li> 30 minutes</li>
+                <li> 1 locations</li>
+                <li> 5+ edited photos per person</li>
+            </ul>
+        </div>
     )
 }
 
 function OtherDescription(props) {
     return ( props.active &&
-        <>
-            other description
-        </>
+        <div className="pricing-info pricing-list">
+            I would love to accomodate your specific photographer needs! Please contact me directly about pricing for miscellaneous and other photo needs!
+        </div>
     )
 }
 
