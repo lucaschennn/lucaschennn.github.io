7165f8a827c0ff9300440958c0364373bb5e1fbc
lucaschennn <lucasch@umich.edu>
2025-01-24 02:45:42 -0500

    less egregious carousel

--- src/components/imgcarousel.jsx
+++ src/components/imgcarousel.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 
 import { Cloudinary } from '@cloudinary/url-gen';
 import { auto } from '@cloudinary/url-gen/actions/resize';
 import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
 import { AdvancedImage } from '@cloudinary/react';
+
+const cld = new Cloudinary({ cloud: { cloudName: 'dch9wtpmk' } });
 
 function ImgCarousel() {
 
@@ -13,36 +15,32 @@ function ImgCarousel() {
     const [feature, setFeature] = useState(0);
     useEffect(() => {
         const interval = setInterval(() => {
-            const timeout = setTimeout(() => {setFeature((feature + 1) % 4);}, 500);
-
+            setFeature((prevFeature) => (prevFeature + 1) % 4);
         }, 2500);
 
-
-
         return () => {clearInterval(interval);}
-    })
+    }, [])
 
     const images = [
-        { id: 0, src: "6_tq9p2x",},
-        { id: 1, src: "13_ptal5d",},
-        { id: 2, src: "DSC04093_y2qvrf",},
-        { id: 3, src: "_27A8236_lnpssb",}
+        { id: 0, src: "images/home/new0.jpg",},
+        { id: 1, src: "images/home/new1.jpg",},
+        { id: 2, src: "images/home/new2.jpg",},
+        { id: 3, src: "images/home/new3.jpg",}
     ]
 
-
-    const cld = new Cloudinary({ cloud: { cloudName: 'dch9wtpmk' } });
-  
-    // Use this sample image or upload your own via the Media Explorer
-    const imgtest = cld
-          .image('13_ptal5d')
-          .format('auto') // Optimize delivery by resizing and applying auto-format and auto-quality
-          .quality('auto')
-  
+    // const cldImgs = useMemo(
+    //     () =>
+    //         images.map((image) =>
+    //             cld.image(image.src).format('auto').quality('auto')
+    //         ),
+    //     [images]
+    // );
     
     return (
         <>
-            {images.map((image) => (
-                <AdvancedImage key={image.id} className={`home-img ${image.id === feature? "active" : "inactive"}`} cldImg={cld.image(image.src).format('auto').quality('auto')}/>
+            {images.map((image, idx) => (
+                // <AdvancedImage key={image.id} className={`home-img ${image.id === feature? "active" : "inactive"}`} cldImg={cldImgs[idx]}/>
+                <img  key={image.id} className={`home-img ${image.id === feature? "active" : "inactive"}`} src={image.src}/>
             ))}
         </>
     );
