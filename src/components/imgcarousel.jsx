3302fc90772aa6877dd6b8811988f326fe3f5c42
lucaschennn <lucasch@umich.edu>
2025-07-04 21:51:55 -0700

    version 2 initial commit

--- src/components/imgcarousel.jsx
+++ src/components/imgcarousel.jsx
@@ -10,16 +10,8 @@ const cld = new Cloudinary({ cloud: { cloudName: 'dch9wtpmk' } });
 
 function ImgCarousel() {
 
-    // const [img, setImg] = useState(0);
     const [imgActive, setImgActive] = useState(true);
     const [feature, setFeature] = useState(0);
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setFeature((prevFeature) => (prevFeature + 1) % 4);
-        }, 2500);
-
-        return () => {clearInterval(interval);}
-    }, [])
 
     const images = [
         { id: 0, src: "images/home/new0.jpg",},
@@ -28,18 +20,19 @@ function ImgCarousel() {
         { id: 3, src: "images/home/new3.jpg",}
     ]
 
-    // const cldImgs = useMemo(
-    //     () =>
-    //         images.map((image) =>
-    //             cld.image(image.src).format('auto').quality('auto')
-    //         ),
-    //     [images]
-    // );
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFeature((prevFeature) => (prevFeature + 1) % images.length);
+        }, 5000);
+
+        return () => {clearInterval(interval);}
+    }, [])
+
     
     return (
         <>
             {images.map((image, idx) => (
-                // <AdvancedImage key={image.id} className={`home-img ${image.id === feature? "active" : "inactive"}`} cldImg={cldImgs[idx]}/>
                 <img  key={image.id} className={`home-img ${image.id === feature? "active" : "inactive"}`} src={image.src}/>
             ))}
         </>
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -3,34 +3,6 @@ import { useState, useEffect, useRef } from 'react'
 import ImgCarousel from '../components/imgcarousel.jsx'
 function Home() {
 
-    const words = [' student', ' photographer', 'n engineer'];
-    const [top, setTop] = useState(true);
-    const [active, setActive] = useState(true);
-    const [desc, setDesc] = useState(0);
-    const contentRef = useRef();
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setActive(false);
-            const timeout = setTimeout(() => {setDesc((desc + 1) % 3); setActive(true);}, 1000);
-
-        }, 2500);
-
-
-
-        return () => {clearInterval(interval);}
-    })
-    /*
-    useEffect(() => {
-        window.onscroll = function() {
-            if(window.pageYOffset === 0) {
-              setTop(true)
-            } else {
-                setTop(false);
-            }
-          };
-    })
-    */
 
     const handleClick = (link) => {
       window.open(link, '_blank')
@@ -38,11 +10,6 @@ function Home() {
 
     return (
       <div id="intro">
-        <p>
-          Hi! My name is Lucas. I am a{!active && <span className="cursor"></span>}
-          <span className={`intro-text ${active ? "active" : ""}`}>{words[desc]}</span>
-          {active && <span className="cursor"></span>}
-        </p>
         <div id="social-btns">
           <button className="social-btn">
             <span style={{fontSize:'24px', color:'black'}} className="fa" onClick={() => {handleClick("https://www.instagram.com/lucaschenphoto/")}}>&#xf16d;</span>
